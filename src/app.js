6ed22ad10971d9ec3285f405c93c20c8506fed6a
Vanessa Ferrer <vanessalimaoi729@gmail.com>
2024-10-23 17:51:01 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -6,6 +5,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const who = ["The dog", "My grandma", "The mailman", "My bird"],
+    action = ["ate", "peed", "crushed", "broke"],
+    what = ["my homework", "my phone", "the car"],
+    when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+  function generateExcuse() {
+    const whos = who[Math.floor(Math.random() * who.length)];
+    const actions = action[Math.floor(Math.random() * action.length)];
+    const whats = what[Math.floor(Math.random() * what.length)];
+    const whens = when[Math.floor(Math.random() * when.length)];
+git add .Math
+    return `${whos} ${actions} ${whats} ${whens}.`;
+  }
+
+  document.getElementById("excuse").innerHTML = generateExcuse();
 };
